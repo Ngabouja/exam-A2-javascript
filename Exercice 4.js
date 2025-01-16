dd7e68d1d08cab71b529f2102e700ca2284a9f74
Ngabouja <ngabouja@edenschool.fr>
2025-01-16 09:07:26 +0100

    feat(app:new commit

---
+++ Exercice 4.js
@@ -0,0 +1,6 @@
+function rectangleArea(length, width) {
+    return( length * width ) // resultat = 10 x 5 = 50
+  }
+  
+  // Exemple de test
+  console.log(rectangleArea(10, 5)); // Output attendu : 50
---
+++ Exercice 5.js
@@ -0,0 +1,7 @@
+function declareConstant() {
+    const Pi = 3.14;
+    return Pi; 
+  }
+  
+  // Exemple de test
+  console.log(declareConstant()); // Output attendu : 3.14
